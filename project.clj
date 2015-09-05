0f0feb127b535c44bc814b5442ee50d3ca74e501
Michael <jaybuffay@aol.com>
2015-09-04 22:34:24 -0500

    #2 revision

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject clojure-server "0.1.0-SNAPSHOT"
+(defproject clojure-server "0.0.1"
   :description "This is a basic clojure web server."
   :url "http://www.emersea.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]]
-  :main main.core
+  :main clojure-server.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
